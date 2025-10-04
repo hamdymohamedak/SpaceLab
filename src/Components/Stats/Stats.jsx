141fdce9aefe0e80406cca4ee6a0ecf3d0ee7b88
askander <mohameddhamdy407@gmail.com>
2025-10-04 17:28:03 +0300

    feat(stats) add translation

--- src/Components/Stats/Stats.jsx
+++ src/Components/Stats/Stats.jsx
@@ -1,39 +1,35 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 export default function Stats() {
+  const {t} = useTranslation()
   return (
     <div className="flex justify-around items-center h-[8rem] py-2 px-4 bg-gradient-to-r from-[#F5A623] via-[#8E2DE2]/80 to-[#4A00E0] text-white border-t-4 border-transparent">
-      {/* Stat 1 */}
-      <div className="text-center">
-        <div className="text-2xl sm:text-3xl md:text-4xl font-bold">1,223</div>
-        <div className="text-xs sm:text-sm md:text-base">Happy Clients</div>
-      </div>
 
-      {/* Vertical Divider */}
-      <div className="w-px h-full bg-white/30 mx-1"></div>
-
-      {/* Stat 2 */}
-      <div className="text-center">
-        <div className="text-2xl sm:text-3xl md:text-4xl font-bold">4</div>
-        <div className="text-xs sm:text-sm md:text-base font-bold">Years of Experience</div>
-      </div>
-
-      {/* Vertical Divider */}
-      <div className="w-px h-full bg-white/30 mx-1"></div>
-
-      {/* Stat 3 */}
-      <div className="text-center">
-        <div className="text-2xl sm:text-3xl md:text-4xl font-bold">850+</div>
-        <div className="text-xs sm:text-sm md:text-base font-bold">Client Reviews</div>
-      </div>
-
-      {/* Vertical Divider */}
-      <div className="w-px h-full bg-white/30 mx-1"></div>
-
-      {/* Stat 4 */}
       <div className="text-center">
         <div className="text-2xl sm:text-3xl md:text-4xl font-bold">4.9</div>
-        <div className="text-xs sm:text-sm md:text-base font-bold">Average Rating</div>
+        <div className="text-xs sm:text-sm md:text-base font-bold">{t("AverageRating")}</div>
+      </div>
+      <div className="w-px h-full bg-white/30 mx-1"></div>
+
+      <div className="text-center">
+        <div className="text-2xl sm:text-3xl md:text-4xl font-bold">850+</div>
+        <div className="text-xs sm:text-sm md:text-base font-bold">{t("ClientReviews")}</div>
+      </div>
+
+      <div className="w-px h-full bg-white/30 mx-1"></div>
+
+
+      <div className="text-center">
+        <div className="text-2xl sm:text-3xl md:text-4xl font-bold">4</div>
+        <div className="text-xs sm:text-sm md:text-base font-bold">{t("YearsOfExperience")}</div>
+      </div>
+      <div className="w-px h-full bg-white/30 mx-1"></div>
+
+
+            <div className="text-center">
+        <div className="text-2xl sm:text-3xl md:text-4xl font-bold">1,223</div>
+        <div className="text-xs sm:text-sm md:text-base">{t("HappyClients")}</div>
       </div>
     </div>
   );
