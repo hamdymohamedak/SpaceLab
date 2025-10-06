b2df657a3a91a47be00fb79b069166b8413f22d6
askander <mohameddhamdy407@gmail.com>
2025-10-06 15:05:35 +0300

    remove analysis

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
   plugins: [
@@ -11,10 +10,6 @@ export default defineConfig({
       },
     }),
     tailwindcss(),
-    visualizer({           
-      open: true,        
-      filename: 'stats.html'
-    }),
   ],
   build: {
     minify: 'esbuild',
